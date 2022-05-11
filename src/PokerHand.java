f6c25412ac5546d086161409e676bebb8c7afa88
hopecris31 <hope.crisafi@gmail.com>
2022-05-10 21:52:40 -0400

    asdfghjkl

--- src/PokerHand.java
+++ src/PokerHand.java
@@ -82,6 +82,34 @@ public class PokerHand {
         return occurrences == 4;
     }
 
+    private String getHandType(){
+        if(this.isFlush()){
+            return "Flush";}
+        if(this.isTwoPair()){
+            return "Two Pair";}
+        if(this.isFourKind()){
+            return "Two Pair";}
+        if(this.isThreeKind()){
+            return "Pair";}
+        if(this.isPair()){
+            return "Pair";}
+        else{
+            return "High Card";}
+    }
+
+    private ArrayList<Integer> getPairs(){
+        ArrayList<Integer> listPairs = new ArrayList<>();
+        if(this.isPair()){
+            for (int card1 = 0; card1 < this.hand.size(); card1++) {
+                for (int card2 = 1; card2 < this.hand.size(); card2++) {
+                    if(this.hand.get(card1).getRank() == this.hand.get(card2).getRank() && !listPairs.contains(this.hand.get(card1).getRank())){
+                            listPairs.add(this.hand.get(card1).getRank());
+                    }
+                }
+            }
+        }
+        return listPairs;
+    }
 
 }
 
